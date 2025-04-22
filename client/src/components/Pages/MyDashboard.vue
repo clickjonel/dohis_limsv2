dbed8cd3a4687f4a3887794840ce7db6988cdd58
clickjonel <139108979+clickjonel@users.noreply.github.com>
2025-04-22 14:36:06 +0000

    Dashboard
    Set Blocks when loading the data on dashboard

--- client/src/components/Pages/MyDashboard.vue
+++ client/src/components/Pages/MyDashboard.vue
@@ -1,8 +1,9 @@
 <template>
     <AuthenticatedPage :pageTitle="'Welcome to your Dashboard'">
         <div v-if="store.getUser.assignment.employee_status_id === 1" class="w-full max-h-[90%] h-full flex justify-start items-center gap-4 p-4 font-lexend">
+        <!-- <div class="w-full max-h-[90%] h-full flex justify-start items-center gap-4 p-4 font-lexend"> -->
             <div class="w-2/3 max-h-full h-full flex flex-col justify-center items-center gap-4">
-                <div class="w-full h-1/2 flex flex-col justify-center items-center gap-4 shadow-md shadow-slate-600 rounded-xl bg-white">
+                <div class="w-full h-1/2 flex flex-col justify-center items-center gap-4 shadow-md shadow-slate-600 rounded-xl bg-white dashboard">
                     <span class="w-full text-2xl uppercase text-center font-noto font-black">Your Deliveries</span>
                     <div class="w-full h-1/3 flex justify-center items-center gap-4 px-4">
                         <div class="w-1/3 h-full flex flex-col justify-center items-center shadow-md shadow-slate-600 p-2 gap-2 rounded-">
@@ -29,7 +30,7 @@
                         </div>
                     </div>
                 </div>
-                <div class="w-full h-1/2 flex flex-col justify-center items-center gap-4 shadow-md shadow-slate-600 rounded-xl bg-white">
+                <div class="w-full h-1/2 flex flex-col justify-center items-center gap-4 shadow-md shadow-slate-600 rounded-xl bg-white dashboard">
                     <span class="w-full text-2xl uppercase text-center font-noto font-black">Your Section Stocks</span>
                     <div class="w-full h-1/3 flex justify-center items-center gap-4 px-4">
                         <div class="w-2/3 h-full flex flex-col justify-center items-center shadow-md shadow-slate-600 p-2 gap-2 rounded-">
@@ -54,7 +55,7 @@
                 </div>
             </div>
             <div class="w-1/3 max-h-full h-full flex flex-col justify-center items-center gap-4">
-                <div class="w-full h-full flex flex-col justify-start items-center gap-4 bg-white rounded-xl">
+                <div class="w-full h-full flex flex-col justify-start items-center gap-4 bg-white rounded-xl dashboard">
                     <span class="w-full text-2xl uppercase text-center font-noto font-black p-2 underline sticky top-0 bg-white rounded-t-xl">Your Properties</span>
 
                     <div class="w-[95%] h-full border-y flex flex-col justify-start items-start font-light gap-2 text-sm overflow-auto">
@@ -73,8 +74,7 @@
     import AuthenticatedPage from '../PageLayouts/AuthenticatedPage.vue';
     import axios from '../../axios/axios.js';
     import { useAuthStore } from '../../stores/authStore.js';
-    import VueApexCharts from 'vue3-apexcharts';
-    import Dialog from 'primevue/dialog';
+    import { Block } from 'notiflix/build/notiflix-block-aio';
 
     const store = useAuthStore()
 
@@ -87,6 +87,10 @@
     })
 
     function fetchDashboardData(){
+        Block.pulse('.dashboard', 'Loading, Please wait...', {
+            fontFamily: 'Lexend Deca',
+        });
+
         axios.get('/dashboard/user')
         .then(response => {
             delivery.value = response.data.delivery
@@ -97,8 +101,10 @@
             console.error(error);
         })
         .finally(() => {
-            //Loading.remove()
+            Block.remove('.dashboard', 1500);
         });
     }
+
+  
 
 </script>
